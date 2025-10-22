77e81d31ad731bb0b0f51e49d269d43d557a56fd
Omar-Tnzxo <omarbusiness100k@gmail.com>
2025-10-22 04:30:49 +0300

    Use esbuild instead of terser for minification

--- vite.config.ts
+++ vite.config.ts
@@ -43,13 +43,7 @@ export default defineConfig({
   assetsInclude: ['**/*.gltf', '**/*.bin', '**/*.glb'],
   build: {
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: 'esbuild',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
